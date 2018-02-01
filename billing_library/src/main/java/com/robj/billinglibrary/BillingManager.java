778bfc8738ae00dd042a8772256ae947f37c6d3d
jj <rob@iamrobj.com>
2018-02-01 09:50:34 +0000

    - Added builder - Added option to change trial length - Added option to reset trial

--- billing_library/src/main/java/com/robj/billinglibrary/BillingManager.java
+++ billing_library/src/main/java/com/robj/billinglibrary/BillingManager.java
@@ -6,7 +6,10 @@ import android.text.TextUtils;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.robj.billinglibrary.PrefsUtils.*;
+import static com.robj.billinglibrary.PrefsUtils.readLongPref;
+import static com.robj.billinglibrary.PrefsUtils.readStringPref;
+import static com.robj.billinglibrary.PrefsUtils.writeLongPref;
+import static com.robj.billinglibrary.PrefsUtils.writeStringPref;
 
 /**
  * Created by jj on 05/11/17.
@@ -16,23 +19,29 @@ public class BillingManager {
 
     private static final String TAG = BillingManager.class.getSimpleName();
 
-    private static final String INSTALL_DATE = "INSTALL_DATE";
+    private static final String TRIAL_START_DATE = "TRIAL_START_DATE";
+    private static final String TRIAL_LENGTH = "TRIAL_LENGTH";
     private static final String PURCHASED_SKU = "PURCHASED_SKU";
     private static final String LAST_PURCHASED_CHECK_DATE = "LAST_PURCHASED_CHECK_DATE";
 
-    private static final long TRIAL_IN_DAYS = 14;
-    private static final long TRIAL_TIME_IN_MILLIS = TimeUnit.DAYS.toMillis(TRIAL_IN_DAYS);
-
-    public static void init(Application context) {
-        getInstallDate(context); //Create the install date for trial if it's not already set
+    protected BillingManager(Application context) {
+        getTrialStartDate(context); //Create the install date for trial if it's not already set
         Billing.init(context);
+    }
+
+    private static long getTrialLengthInMillis(Context context) {
+        return readLongPref(context, TRIAL_LENGTH);
+    }
+
+    protected void setTrialLengthInDays(Context context, int trialLengthInDays) {
+        writeLongPref(context, TRIAL_LENGTH, TimeUnit.DAYS.toMillis(trialLengthInDays));
     }
 
     public static boolean isTrialPeriod(Context context) {
         if(isPurchased(context))
             return false;
-        long diff = System.currentTimeMillis() - getInstallDate(context);
-        return diff < TRIAL_TIME_IN_MILLIS;
+        long diff = System.currentTimeMillis() - getTrialStartDate(context);
+        return diff < getTrialLengthInMillis(context);
     }
 
     public static boolean hasPaidFeatures(Context context) {
@@ -42,22 +51,22 @@ public class BillingManager {
     public static int getTrialPeriodLeft(Context context) {
         if(!isTrialPeriod(context))
             return 0;
-        long diff = System.currentTimeMillis() - getInstallDate(context);
-        diff = TRIAL_TIME_IN_MILLIS - diff;
+        long diff = System.currentTimeMillis() - getTrialStartDate(context);
+        diff = getTrialLengthInMillis(context) - diff;
         return (int) Math.ceil((double) diff/86400000);
     }
 
-    public static long getInstallDate(Context context) {
-        String installDate = readStringPref(context, INSTALL_DATE);
-        if(TextUtils.isEmpty(installDate)) {
-            installDate = String.valueOf(System.currentTimeMillis());
-            setInstallDate(context, installDate);
+    public static long getTrialStartDate(Context context) {
+        long installDate = readLongPref(context, TRIAL_START_DATE);
+        if(installDate == 0) {
+            installDate = System.currentTimeMillis();
+            setTrialStartDate(context, installDate);
         }
-        return Long.valueOf(installDate);
+        return installDate;
     }
 
-    static void setInstallDate(Context context, String installDate) {
-        writeStringPref(context, INSTALL_DATE, installDate);
+    static void setTrialStartDate(Context context, long installDateInMillis) {
+        writeLongPref(context, TRIAL_START_DATE, installDateInMillis);
     }
 
     public static boolean isPurchased(Context context) {
@@ -79,6 +88,38 @@ public class BillingManager {
 
     static void setLastPurchaseCheckedDate(Context context, long dateInMillis) {
         writeLongPref(context, LAST_PURCHASED_CHECK_DATE, dateInMillis);
+    }
+
+    public static class Builder {
+
+        private final Application context;
+        private int trialLengthInDays = 14;
+        private boolean resetTrial = false;
+
+        public Builder(Application context) {
+            this.context = context;
+        }
+
+        public Builder setTrialLength(int trialLengthInDays) {
+            if(trialLengthInDays <= 0)
+                throw new RuntimeException("Trial length needs to be 1 day or more - default is 14");
+            this.trialLengthInDays = trialLengthInDays;
+            return this;
+        }
+
+        public Builder resetTrial(boolean resetTrial) {
+            this.resetTrial = resetTrial;
+            return this;
+        }
+
+        public BillingManager build() {
+            BillingManager billingManager = new BillingManager(context);
+            billingManager.setTrialLengthInDays(context, trialLengthInDays);
+            if(resetTrial)
+                BillingManager.setTrialStartDate(context, System.currentTimeMillis());
+            return billingManager;
+        }
+
     }
 
 }
